f06d72c6424519347d7612235c0fdcd6d1857cd9
dokanelbalad <dokanelbalad@gmail.com>
2026-09-20 00:42:56 +0300

    السماح بالدومين الحي في CORS

--- config/cors.php
+++ config/cors.php
@@ -1,21 +1,24 @@
 <?php
- 
+
 return [
- 
+
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
- 
+
     'allowed_methods' => ['*'],
- 
-    'allowed_origins' => ['http://localhost:3000'],
- 
+
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'https://dokanelbalad-web.vercel.app',
+    ],
+
     'allowed_origins_patterns' => [],
- 
+
     'allowed_headers' => ['*'],
- 
+
     'exposed_headers' => [],
- 
+
     'max_age' => 0,
- 
+
     'supports_credentials' => false,
- 
+
 ];
